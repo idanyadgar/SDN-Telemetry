931c49be891d62321a88408b3646fbe0c1257fe1
idanyadgar <idanyadgar@gmail.com>
2018-05-19 18:19:43 +0300

    Added default topology and an agent that prints a message when a packet comes in

---
+++ defaultTopo.py
@@ -0,0 +1,47 @@
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import (OVSSwitch,Agent)
+from ourAgent import OurAgent
+
+class MyTopo(Topo):
+    def build(self):
+        s0 = self.addSwitch('s0')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        self.addLink(s0, s2)
+        self.addLink(s0, s3)
+        self.addLink(s1, s2)
+
+        h1 = self.addHost('laptop')
+        h3 = self.addHost('printer')
+        h2 = self.addHost('pc')
+
+        self.addLink(h1, s0)
+        self.addLink(h2, s3)
+        self.addLink(h3, s1)
+
+        a0 = self.addAgent('a0')
+        a1 = self.addAgent('a1')
+        a2 = self.addAgent('a2')
+        a3 = self.addAgent('a3')
+
+        self.attach(a0, s0)
+        self.attach(a1, s1)
+        self.attach(a2, s2)
+        self.attach(a3, s3)
+
+def main():
+    topo = MyTopo()
+    net = Mininet(topo = topo, switch = OVSSwitch, controller = None, agent = OurAgent, autoSetMacs = True)
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    main()
+
---
+++ ourAgent.py
@@ -0,0 +1,19 @@
+from mininet.node import Agent
+from mininet.util import quietRun
+
+class OurAgent(Agent):
+    def __init__(self, name, *ryuArgs, **kwargs):
+        ourApp = 'agentApp.py'
+
+        currDir = quietRun('pwd').strip('\r\n')
+        ryuCoreDir = '%s/' % currDir
+
+        if not ryuArgs:
+            ryuArgs = [ryuCoreDir + ourApp]
+        elif type(ryuArgs) not in (list, tuple):
+            ryuArgs = [ryuArgs]
+
+        Agent.__init__(self, name,  command='ryu-manager --verbose',
+                                    cargs='--ofp-tcp-listen-port %s ' + ' '.join(ryuArgs),
+                                    cdir=ryuCoreDir, **kwargs)
+
